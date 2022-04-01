692a96196a4fb324150a92059de81c8950e8672d
Anton Turchenko <antont@default-value.com>
2022-04-01 11:10:21 +0300

    added test with unrolled iteration names

--- src/test/groovy/com/antont/spocktests/SpockDataDrivenSpec.groovy
+++ src/test/groovy/com/antont/spocktests/SpockDataDrivenSpec.groovy
@@ -22,11 +22,15 @@ class SpockDataDrivenSpec extends Specification {
         SimpleService simpleService = new SimpleService()
         expect:
         simpleService.mergeStrings(a, b)
+        where:
+         a  |  b  |  c
+        "a" | "b" | "ab"
+        "1" | "2" | "12"
 
     }
 
     @Rollup // will run as one test
-    def "simple test with data table"(String a, String b) {
+    def "simple test rollup with data driven"(String a, String b) {
         given:
         SimpleService simpleService = new SimpleService()
         expect:
@@ -38,5 +42,19 @@ class SpockDataDrivenSpec extends Specification {
         "test"                         | " string" |  "test string"        // simple test with shared
         simpleService.getSharedField() | " 1"      | "shared value 1"  // simple data table test with @Shared field
         "test"                         | a + " 2"  |  "test test 2"        // test with accessing other data variable
+    }
+
+    @Unroll("full name: #c")
+    def "combine first and second name"(String a, String b) {
+        given:
+        SimpleService simpleService = new SimpleService()
+        expect:
+        simpleService.mergeStrings(a, b)
+
+
+        where:
+        a          |            b           |          c
+        "Anton"    |    " Turchenko"        |       "Anton Turchenko"
+        "Aragorn"  | " the son of Arathorn" | "Aragorn the son of Arathorn"
     }
 }
